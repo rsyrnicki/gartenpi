f0567f803a4bc6a2d53d5c7410c43367a21b81fd
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-23 14:19:47 +0200

    corrected typo

--- appTimers.py
+++ appTimers.py
@@ -6,7 +6,7 @@ def timedToggles(button, startH, endH, interval):
 	
 	while True:
 		t = time.localtime()
-		hours = int(time.striftime("%H", t)
+		hours = int(time.striftime("%H", t))
 		minutes = int(time.strftime("%M", t))
 		seconds = int(time.strftime("%S", t))
 		# Potentiall error, because I might have messed something up

227896dbd24919944094fd0c8b05ca2f391b342f
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-23 14:20:24 +0200

    corrected typo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ try:
 	# Start Threads
 	thread.start_new_thread(appCore.runGUI)
 	thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 8, 21, 15))
-	thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 0, 24, 1))
+	thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 0, 24, 2))
 except:
 	print("A Thread failed")
 

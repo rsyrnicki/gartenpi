ecde7db5393e9bddda229c4b9b24ae5dde8ab7b0
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-26 00:32:36 +0200

    Multithreading Timers added

--- app.py
+++ app.py
@@ -1,43 +1,5 @@
 #!/usr/bin/python3
 
-import threading
-import time
-
 import appCore
-import appTimers
-
-exitFlag = 0
-
-class timerThread (threading.Thread):
-   #def __init__(self, threadID, name, counter, btn, start, stop, interv):
-	def __init__(self, threadID, name, counter):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.counter = counter
-		#self.btn = btn
-		#self.start = start
-		#self.stop = stop
-		#self.interv = interv
-	def run(self):
-		print ("Starting " + self.name)
-		#appTimers.timedToggles(self.name, self.btn, self.start, self.stop, self.interv)
-		appTimers.timedToggles(self.name, appCore.buttons[1], 0, 24, 2)
-		print ("Exiting " + self.name)
-
-# Create new threads
-#threadTimer1 = timerThread(1, "Thread-1", 1, appCore.buttons[0], 8, 21, 15)
-#threadTimer2 = timerThread(2, "Thread-2", 2, appCore.buttons[1], 0, 24, 2)
-testThread = timerThread(1, "Thread-1", 1)
-
-# Start new Threads
-#threadTimer1.start()
-#threadTimer2.start()
-#threadTimer1.join()
-#threadTimer2.join()
-testThread.start()
-testThread.join()
-
-appCore.runGUI()
 
 print ("Exiting Main Thread")

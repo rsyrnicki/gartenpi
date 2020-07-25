5c3f474350a0facd345dd071a4b3f0128957ca90
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-25 21:27:19 +0200

    test Thread

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ exitFlag = 0
 class timerThread (threading.Thread):
    #def __init__(self, threadID, name, counter, btn, start, stop, interv):
 	def __init__(self, threadID, name, counter, btn, start, stop, interv):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-      #self.btn = btn
-      #self.start = start
-      #self.stop = stop
-      #self.interv = interv
-   def run(self):
-      print ("Starting " + self.name)
-      #appTimers.timedToggles(self.name, self.btn, self.start, self.stop, self.interv)
-      appTimers.timedToggles(self.name, appCore.buttons[1], 0, 24, 2)
-      print ("Exiting " + self.name)
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+		#self.btn = btn
+		#self.start = start
+		#self.stop = stop
+		#self.interv = interv
+	def run(self):
+		print ("Starting " + self.name)
+		#appTimers.timedToggles(self.name, self.btn, self.start, self.stop, self.interv)
+		appTimers.timedToggles(self.name, appCore.buttons[1], 0, 24, 2)
+		print ("Exiting " + self.name)
 
 # Create new threads
 #threadTimer1 = timerThread(1, "Thread-1", 1, appCore.buttons[0], 8, 21, 15)

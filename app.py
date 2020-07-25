6a44d975bc0fdae5e4ac3dc3506c9f0cdd446722
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-25 20:18:26 +0200

    threading update

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
+#!/usr/bin/python3
+
+import threading
+import time
+
 import appCore
 import appTimers
 
-import _thread
+exitFlag = 0
 
-try:
-	# Start GUI Thread
-	_thread.start_new_thread(appCore.runGUI)
-except:
-	print("A GUI Thread failed")
-	
-try:
-	# Start Timer Threads
-	_thread.start_new_thread(appTimers.timedToggles, appCore.buttons[0], 8, 21, 15)
-	_thread.start_new_thread(appTimers.timedToggles, appCore.buttons[0], 0, 24, 2)
-except:
-	print("A Timer Thread failed")
+class guiThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      appCore.runGUI()
+      print ("Exiting " + self.name)
 
-while True:
-	pass
+class timerThread (threading.Thread):
+   def __init__(self, threadID, name, counter, btn, start, stop, interv):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      appTimers.timedToggles(btn, start, stop, interv)
+      print ("Exiting " + self.name)
+
+
+
+# Create new threads
+threadGUI = newThread(1, "Thread-1", 1)
+threadTimer1 = newThread(2, "Thread-2", 2, appCore.buttons[0], 8, 21, 15)
+threadTimer1 = newThread(3, "Thread-3", 3, appCore.buttons[1], 0, 24, 2)
+
+# Start new Threads
+threadGUI.start()
+threadTimer1.start()
+threadTimer2.start()
+threadGUI.join()
+threadTimer1.join()
+threadTimer2.join()
+print ("Exiting Main Thread")

0d39fa93d61117a821b4e96085c62a2a0f3edfdf
Robert Syrnicki <r_syrnicki18@cs.uni-kl.de>
2020-07-25 19:51:54 +0200

    syntax

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import _thread
 
 try:
 	# Start Threads
-	thread.start_new_thread(appCore.runGUI)
-	thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 8, 21, 15))
-	thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 0, 24, 2))
+	_thread.start_new_thread(appCore.runGUI)
+	_thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 8, 21, 15))
+	_thread.start_new_thread(appTimers.timedToggles(appCore.buttons[0], 0, 24, 2))
 except:
 	print("A Thread failed")
 
